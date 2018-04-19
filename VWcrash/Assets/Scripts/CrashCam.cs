c11df7954eb0b0d56ed9e5e2ac42276e29904995
jbishop-art <jonathan.bishop.art@gmail.com>
2018-04-18 21:53:21 -0600

    Camera working as intended forward and backward

--- VWcrash/Assets/Scripts/CrashCam.cs
+++ VWcrash/Assets/Scripts/CrashCam.cs
@@ -8,10 +8,19 @@ public class CrashCam : MonoBehaviour
 	public float xOffsetLeft;
 	public float xOffsetRight;
 	public float cameraDistance;
+	public float cameraDistanceOffset;
 	public float cameraHeight;
-	public float dampTime = 0.15f;
+	public float dampTimeForward = 0.15f;
+	public float dampTimeBackward = 0.15f;
+	private float z;
 	private Vector3 velocity = Vector3.zero;
 
+	private float movement;
+	private float prevPos;
+	private float newPos;
+	//private float fwd;
+	private float moveResult;
+	
 	// Use this for initialization
 	void Start () 
 	{
@@ -19,29 +28,63 @@ public class CrashCam : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	void FixedUpdate () 
 	{
-		float y = Player.CurrentPlayer.transform.position.y + cameraHeight;  // Ccamera a bit heigher than the character.  Or -2, depends on from side you want to look.
-		float z = Player.CurrentPlayer.transform.position.z - cameraDistance;  // Distance between character and camera.
+		float y = Player.CurrentPlayer.transform.position.y + cameraHeight;  // Camera a bit heigher than the character.  
+		
+		newPos = (Mathf.Round(Player.CurrentPlayer.transform.position.z * 10) / 10);
+		movement = (newPos - prevPos);
+		
+		Debug.Log(movement);
 
-		if (Player.CurrentPlayer.transform.position.x > 2.5)
+		if (movement < 0)  // Checks to see if player is moving toward camera.
 		{
-			Vector3 destination = new Vector3 (xOffsetRight, y, z);
-			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+			z = Player.CurrentPlayer.transform.position.z - (cameraDistance + cameraDistanceOffset);  // Distance between character and camera  will be offset.
+
+			if (Player.CurrentPlayer.transform.position.x > 2.5) //player going Right.
+			{
+				Vector3 destination = new Vector3 (xOffsetRight, y, z);
+				transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTimeBackward);
+			}
+			else if (Player.CurrentPlayer.transform.position.x < -2.5) //Player going Left.
+			{
+				Vector3 destination = new Vector3 (xOffsetLeft, y, z);
+				transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTimeBackward);
+			}
+			else //Player is in Center.
+			{
+				Vector3 destination = new Vector3 (xCenter, y, z);
+				transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTimeBackward);
+			}
 		}
-		else if (Player.CurrentPlayer.transform.position.x < -2.5)
+		else // Player is moving away from camera.
 		{
-			Vector3 destination = new Vector3 (xOffsetLeft, y, z);
-			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
-		}
-		else 
-		{
-			Vector3 destination = new Vector3 (xCenter, y, z);
-			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+			z = Player.CurrentPlayer.transform.position.z - cameraDistance ; // Player and camera distance will be normal.
+
+			if (Player.CurrentPlayer.transform.position.x > 2.5) //Player going Right.
+			{
+				Vector3 destination = new Vector3 (xOffsetRight, y, z);
+				transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTimeForward);
+			}
+			else if (Player.CurrentPlayer.transform.position.x < -2.5)  //Player going Left.
+			{
+				Vector3 destination = new Vector3 (xOffsetLeft, y, z);
+				transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTimeForward);
+			}
+			else //Player is in Center.
+			{
+				Vector3 destination = new Vector3 (xCenter, y, z);
+				transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTimeForward);
+			}
 		}
 
-		//transform.position = new Vector3 (xCenter, y, z);
-		transform.rotation = new Quaternion(0, 0, 0, 0);  //rotate around y-Axis to at player.
+		transform.rotation = new Quaternion(0, 0, 0, 0);  //Camer look at player.
 
+	}
+
+	void LateUpdate()
+	{
+		prevPos = (Mathf.Round(Player.CurrentPlayer.transform.position.z * 10) / 10);
+		//fwd = Player.CurrentPlayer.transform.forward.z;
 	}
 }
